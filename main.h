d31167facc3a0c03d7456e9ce67a291fbd4d3870
IsmaelBuere <7674@holbertonstudents.com>
2023-11-13 16:11:31 -0800

    printf

---
+++ main.h
@@ -0,0 +1,23 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+
+// Libraries
+
+#include <stdarg.h>
+#include <stdlib.h>
+#include <stdio.h>
+
+
+// ----------------
+
+
+
+
+// _printf
+
+
+int _printf(const char *format, ...);
+
+
+#endif
